1a8c38d562f859d9c22866bcbd9f590017de86d0
VV <vyatkinvladislav@gmail.com>
2020-02-25 22:58:47 +0200

    Exercise 13 solution.

--- 11-20/main.hs
+++ 11-20/main.hs
@@ -14,24 +14,38 @@ encodeModified :: Eq a => [a] -> [Encoded a]
 encodeModified xs = map (\(len, val) -> if len == 1 then Single val else Multiple len val) (encode xs)
 
 decodeModified :: [Encoded a] -> [[a]]
-decodeModified = map (decode)
+decodeModified = map decode
   where decode (Single a) = [a]
         decode (Multiple n a) = replicate n a
 
-encodeDirect :: [a] -> [Encoded a]
+encodeDirect :: Eq a => [a] -> [Encoded a]
 encodeDirect [] = []
 encodeDirect [x] = [Single x]
-encodeDirect (x:xs) = if x == (head (encodeDirect xs))
+encodeDirect (x:xs) = if x == decodeSymbol (head (encodeDirect xs))
+                      then encodeSymbol (1 + (decodeNum . head . encodeDirect $ xs)) x:(tail . encodeDirect $ xs)
+                      else Single x:encodeDirect xs
+  where
+    encodeSymbol 1 y = Single y
+    encodeSymbol n y = Multiple n y
+    decodeSymbol (Single y) = y
+    decodeSymbol (Multiple _ y) = y
+    decodeNum (Single _) = 1
+    decodeNum (Multiple n _) = n
+    
+    
 
 main = do
   putStrLn "Exercises from: https://wiki.haskell.org/99_questions/11_to_20"
 
   putStrLn "-------E11"
-  putStrLn . show $ encodeModified "aaaabccaadeeee"
+  print $ encodeModified "aaaabccaadeeee"
 
   putStrLn "-------E12"
-  putStrLn . show $ decodeModified 
+  print $ decodeModified
        [Multiple 4 'a',Single 'b',Multiple 2 'c',
         Multiple 2 'a',Single 'd',Multiple 4 'e']
 
-  putStrLn . show $ encodeDirect "aaaabccaadeeee"
+  putStrLn "-------E13"
+  print $ encodeDirect "aaaabccaadeeee"
+
+  
